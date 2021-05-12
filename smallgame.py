e6ff4bafb2adbc75e7a6f53ca3b483d691d17aef
Rajender_Reddy <69648580+Rajender-Reddy-8055@users.noreply.github.com>
2021-05-12 12:44:36 +0530

    Update smallgame.py

--- smallgame.py
+++ smallgame.py
@@ -1,60 +1,59 @@
 import random
-L = ["SNAKE", "WATER", "GUN"]
-i = 0
-U = 0
-AI = 0
-L[0] = 1
-L[1] = 2
-L[2] = 3
+L = ["scissor","paper","rock"]
+i = U = D = AI = 0
 while i < 10:
     x = random.choice(L)
-    print("Choose any: \n 1 for SNAKE...2 for WATER...3 for GUN")
-    y = input()
+    y = input("Choose any: \n rock\t paper\t scissor\n")
+    print(f"You choose {y}")
+
     if y == L[0]:
+        if x == L[0]:
+            print(f"Computer choose {x}")
+            print("draw")
+            D = D+1
+        elif x == L[1]:
+            U = U+1
+            print(f"Computer choose {x}")
+            print("you won")
+        else:
+            print(f"Computer choose {x}")
+            print("you lost")
+            AI = AI+1
         print(f"turns left {9-i}")
-        for c in L:
-            if x == L[0]:
-                print("draw")
-                break
-            elif x == L[1]:
-                U = U+1
-                break
-                print("you won")
-            else:
-                print("you lost")
-                AI = AI+1
-                break
     elif y == L[1]:
+        if x == L[0]:
+            print(f"Computer choose {x}")
+            print("you lost")
+            AI = AI+1
+        elif x == L[1]:
+            print(f"Computer choose {x}")
+            print("draw")
+            D = D+1
+        else:
+            print(f"Computer choose {x}")
+            print("you won")
+            U = U+1
         print(f"turns left {9 - i}")
-        for c in L:
-            if x == L[0]:
-                print("you lost")
-                AI = AI+1
-                break
-            elif x == L[1]:
-                print("draw")
-                break
-            else:
-                print("you won")
-                U = U+1
-                break
-    else:
+    elif y == L[2]:
+        if x == L[0]:
+            print(f"Computer choose {x}")
+            print("you won")
+            U = U+1
+        elif x == L[1]:
+            print(f"Computer choose {x}")
+            print("you lost")
+            AI = AI + 1
+        else:
+            print(f"Computer choose{x}")
+            print("draw")
+            D = D+1
         print(f"turns left {9 - i}")
-        for c in L:
-            if x == L[0]:
-                print("you won")
-                U = U+1
-                break
-            elif x == L[1]:
-                print("you lost")
-                AI = AI + 1
-                break
-            else:
-                print("draw")
-                break
+    else :
+        print("Choose wrong option...try again")
+        continue           
     i = i+1
     continue
-print(f"your score = {U}  and ai score = {AI}")
+print(f"your score = {U} , computer score = {AI} and draw matches ={D}")
 if U > AI:
     print("you won....!")
 elif U < AI:
